84eb072eb61fa7ff3a9c38cd6a8e3ead4551185a
Rafael Magossi Queiroz <rafael_mq2@hotmail.com>
2016-03-22 09:50:33 -0400

    Versao 1.2
    Alteração:
    
            Menu com JOptionPane e funções de inserir vetor, remover do heap e etc...
    
    Arrumar:
            quando aperta cancela, objeto nulo

--- src/Heap.java
+++ src/Heap.java
@@ -1,21 +1,46 @@
+import static java.lang.StrictMath.pow;
+
 /**
  * Created by RafaelMq on 07/03/2016.
  */
-public class Heap {
+public class Heap  {
 
 
+    int n;
+    int elemento[];
+
+    Heap() {
+        this.n = 0;
+        elemento = new int[100];
+    }
+
+    public void insereDesordenado(Heap heap, int valor) {
+        heap.n = heap.n + 1;
+        heap.elemento[heap.n] = valor;
+        //heap.n += 1;
+
+        for(int i=0; i<heap.n+1; i++) { //+1 por caus da raiz
+            System.out.print(" | "+heap.elemento[i]);
+
+        }
+        System.out.println("\n");
+    }
 
 
-
-    public void insere(NoHeap heap, int valor) {
+    public void insere(Heap heap, int valor) {
         heap.n = heap.n + 1;
         heap.elemento[heap.n] = valor;
         //heap.n += 1;
         subir(heap, heap.n);
 
+        for(int i=0; i<heap.n+1; i++) { //+1 por caus da raiz
+            System.out.print(" | "+heap.elemento[i]);
+
+        }
+        System.out.println("\n");
     }
 
-    public void subir (NoHeap heap, int posicao){
+    public void subir (Heap heap, int posicao){
         if(posicao > 1){
             int pai;
             pai = posicao/2;
@@ -29,20 +54,21 @@ public class Heap {
         }
     }
 
-    public void remove(NoHeap heap){
+    public int remove(Heap heap){
+        int aux=0;
+
         if(heap.n == 0){
             System.out.println("Impossivel Remover - Heap Vazio");
         }else{
-            int aux;
             aux = heap.elemento[1];
             heap.elemento[1] = heap.elemento[heap.n];
             heap.n -= 1;
             descer(heap,1);
         }
-        // retornar chave removida
+        return aux;
     }
 
-    public void descer(NoHeap heap, int posicao){
+    public void descer(Heap heap, int posicao){
         int filho;
         filho = 2*posicao;
 
@@ -64,7 +90,7 @@ public class Heap {
 
     }
 
-    public void constroi_heap(NoHeap heap, int posicao){
+    public void constroi_heap(Heap heap, int posicao){
         int filho;
         filho = 2*posicao;
         if(filho <= heap.n ){
@@ -77,8 +103,25 @@ public class Heap {
         descer(heap, posicao);
     }
 
-    public void mostraHeap(){
+    String imprimeHeap( Heap heap) {
+        StringBuffer saida = new StringBuffer();
+        int i=1; // armazena a posição que estou imprimindo, começa no 1 pois o v[0] armazena a quantidade de elementos
+        int aux; // armazena quantos elementos ja foram impressos em casa linha
+        int j=0; // armazena a quantidade de linhas que ja foram impressas
+        while(i<=heap.n){ //execulta até atingir a quantidade mazima de elementos
+            for(int espaco = heap.n-i ; espaco>0 ; --espaco){//dar espaco
+                saida.append("  ");
+            }
 
+            for(aux = 0; aux+1 <= (int)pow(2,j) && i <= heap.n; i++){//for para imprimir cada linha
+                saida.append(heap.elemento[i] + "  ");//imprimo h[i]
+                aux++;
+            }
+            saida.append("\n");//enter
+            j++;
+        }
+        return saida.toString();
     }
+
 
 }
--- src/UsaHeap.java
+++ src/UsaHeap.java
@@ -1,3 +1,7 @@
+import javax.swing.*;
+import javax.swing.text.StringContent;
+import javax.swing.text.html.parser.Parser;
+
 /**
  * Created by RafaelMq on 07/03/2016.
  */
@@ -6,17 +10,84 @@ public class UsaHeap {
     public static void main(String[] args){
 
         Heap objHeap = new Heap();
-        NoHeap objNoHeap = new NoHeap();
+        Heap objNaoHeap = new Heap();
 
 
+
+        // int vetorInsereHeap[] = {18,25,41,34,15,11,52,50,48};
         int vetorInsereHeap[] = {18,25,41,34,15,11,52,50,48};
 
         for(int i=0; i<vetorInsereHeap.length; i++) {
-            objHeap.insere(objNoHeap, vetorInsereHeap[i]);
+            objHeap.insere(objHeap, vetorInsereHeap[i]);
         }
 
-        for(int i=0; i<vetorInsereHeap.length; i++) {
-            System.out.print(" | "+objNoHeap.elemento[i]);
+
+
+
+        while(true) {
+
+            int op, auxI;
+            String vetorHeap[];
+            String auxS;
+
+
+            op = Integer.parseInt(JOptionPane.showInputDialog("\nOpções: " +
+
+                    "\n\n" +
+                    "\n 1 - Inserir no Heap" +
+                    "\n 2 - Remover do Heap" +
+                    "\n 3 - Inserir um Vetor nao Heap" +
+                    "\n 4 - Constroi Heap do vetor" +
+                    "\n\n" +
+
+                    objHeap.imprimeHeap(objHeap) + "\n\n")
+
+
+            );
+
+            switch (op) {
+
+                case 1:
+
+                    auxI = Integer.parseInt(JOptionPane.showInputDialog(null,"\nDigite o Valor da Chave: \n\n"));
+                    objHeap.insere(objHeap, auxI);
+                    System.out.println(objHeap.imprimeHeap(objHeap));
+                    break;
+
+                case 2:
+
+                    auxI = objHeap.remove(objHeap);
+                    JOptionPane.showMessageDialog(null,"Chave Removida: " + auxI);
+                    System.out.println(objHeap.imprimeHeap(objHeap));
+
+                break;
+
+                case 3:
+
+                    auxS = (JOptionPane.showInputDialog(null,"\nDigite o Valor da Chave: \n\n"));
+                    vetorHeap = auxS.split(" ");
+                    for (int i=0 ; i<vetorHeap.length ; i++){
+
+                        auxI = Integer.parseInt(vetorHeap[i]);
+                        objNaoHeap.insereDesordenado(objNaoHeap, auxI);
+                    }
+
+                    System.out.println(objNaoHeap.imprimeHeap(objNaoHeap));
+                    break;
+
+                case 4:
+
+                    objNaoHeap.constroi_heap(objNaoHeap,1);
+                    System.out.println(objNaoHeap.imprimeHeap(objNaoHeap));
+                    break;
+
+                default:
+
+                    JOptionPane.showMessageDialog(null,"Numero Invalido !!","Erro", JOptionPane.ERROR_MESSAGE);
+                    break;
+
+
+            }
         }
 
     }
